0f3dfd46e547a7dc3354957b8613c0748ca87f1a
saim2001 <saimrao49@gmail.com>
2023-10-21 22:55:20 +0500

    whitenoise added

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\saim rao\Desktop\Interview\myenv
+set VIRTUAL_ENV=C:\Users\saim rao\Desktop\Interview\Django-rest-API\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
